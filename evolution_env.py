866e1d55a367e498cdfc5fe4ec3b04051f74ba91
Luke <lguerdan@yahoo.com>
2018-12-09 16:59:12 -0600

    make indent uniform 3-spaces

--- evolution_env.py
+++ evolution_env.py
@@ -4,108 +4,108 @@ import numpy as np
 
 ##The below class inherits everything from MSBGeneticOptimizerEnv, all you will need to do is modi
 class EvolutionEnv(MSBGeneticOptimizerEnv):
-    # Arguments: max_steps=10000, num_chromosomes=4, action_encoding=SIMPLE_MOVEMENT, render=False, reward="score", session_file=""
-    def __init__(self, *args, **kwargs):
-        super(EvolutionEnv, self).__init__(*args, **kwargs)
+   # Arguments: max_steps=10000, num_chromosomes=4, action_encoding=SIMPLE_MOVEMENT, render=False, reward="score", session_file=""
+   def __init__(self, *args, **kwargs):
+      super(EvolutionEnv, self).__init__(*args, **kwargs)
 
-    def new_generation(self):
-        """
-        Based on a chromosomes structure, updates the chromosomes by natural selection rules
-        This is where the bulk of the evolutionary computation code will go
-        We will need to modify the chromosome structure in some
-        """
+   def new_generation(self):
+      """
+      Based on a chromosomes structure, updates the chromosomes by natural selection rules
+      This is where the bulk of the evolutionary computation code will go
+      We will need to modify the chromosome structure in some
+      """
         
-        #tournament selection for parent
-        parents = self.select_parents(1, int(self.num_chromosomes/2))
-        offspring = []
+      #tournament selection for parent
+      parents = self.select_parents(1, int(self.num_chromosomes/2))
+      offspring = []
 
-        # Crossover
-        #for (parent1, parent2) in parents:
-        #    offspring.extend(self.crossover_chromosome_pair(parent1, parent2, points_before_death=False))
+      # Crossover
+      #for (parent1, parent2) in parents:
+      #    offspring.extend(self.crossover_chromosome_pair(parent1, parent2, points_before_death=False))
+
+      # Mutation
+      #using lambda = mu (numberof parents equal to the number of offsprings)
+      #For each selected parent, will mutate around 20% of the max_steps actions
+      mutations = round(0.2 * self.max_steps)
+      for chromosome in parents:
+         child = [chromosome[0].copy(), -1, chromosome[2]]
+         self.mutate_chromosome(child, mutations)
+         offspring.append(child)
+
+      # (mu, lambda) Using only offspring to populate new generation here
+      # self.chromosomes = offspring
+
+      #(mu + lambda) Using parents + offspring to populate new generation here
+      self.chromosomes = parents + offspring
+
+   ###
+   # Basic implementation of parent selection, with chromosomes shuffled then returned in pairs
+   # Selection_type: 0: shuffle, 1: tournament
+   ###
+   def select_parents(self, selection_type=0, mu=1):
+      def shuffle_selection():
+         np.random.shuffle(self.chromosomes)
+         parents = []
+         for i in range(int(len(self.chromosomes) / 2)):
+            parents.append((self.chromosomes[2 * i], self.chromosomes[2 * i + 1]))
+         return parents
+      def tournament_selection(mu):
+         parents = []
+         #select mu best chromosomes
+         best_chromosomes_index = sorted(range(len(self.chromosomes)), key=lambda x: self.chromosomes[x][1], reverse=True)
+         #delete from chromosome list if it is not within the mu best
+         for i in range(mu):
+            parents.append(self.chromosomes[best_chromosomes_index[i]])
+         return parents
+
+      if selection_type == 0:
+         return shuffle_selection()
+      elif selection_type == 1:
+         return tournament_selection(mu)
         
-        # Mutation
-        #using lambda = mu (numberof parents equal to the number of offsprings)
-        #For each selected parent, will mutate around 20% of the max_steps actions
-        mutations = round(0.2 * self.max_steps)
-        for chromosome in parents:
-            child = [chromosome[0].copy(), -1, chromosome[2]]
-            self.mutate_chromosome(child, mutations)
-            offspring.append(child)
-        
-        # (mu, lambda) Using only offspring to populate new generation here
-        #self.chromosomes = offspring
+   def crossover_chromosome_pair(self, parent1, parent2, point_num=1, points_before_death=False, normal_dist=False):
+      ###
+      #	Implementation of crossover functionality
+      #	- points: number of points to use in crossover
+      #	- points_before_death: only consider points upto sooner death
+      #	- normal_dist: sample crossover points from normal distribution
+      ###
+      def positive_normal():
+         x = (np.random.randn() / 3)
+         return max(1 - max(x, -x), 0)
 
-        # (mu + lambda) Using parents + offspring to populate new generation here
-        self.chromosomes = parents + offspring
-
-    ###
-    # Basic implementation of parent selection, with chromosomes shuffled then returned in pairs
-    # Selection_type: 0: shuffle, 1: tournament
-    ###
-    def select_parents(self, selection_type=0, mu=1):
-        def shuffle_selection():
-            np.random.shuffle(self.chromosomes)
-            parents = []
-            for i in range(int(len(self.chromosomes) / 2)):
-                parents.append((self.chromosomes[2 * i], self.chromosomes[2 * i + 1]))
-            return parents
-        def tournament_selection(mu):
-            parents = []
-            #select mu best chromosomes
-            best_chromosomes_index = sorted(range(len(self.chromosomes)), key=lambda x: self.chromosomes[x][1], reverse=True)
-            #delete from chromosome list if it is not within the mu best
-            for i in range(mu):
-                parents.append(self.chromosomes[best_chromosomes_index[i]])
-            return parents
-       
-        if selection_type == 0:
-            return shuffle_selection()
-        elif selection_type == 1:
-            return tournament_selection(mu)
-        
-    ###
-    #	Implementation of crossover functionality
-    #	- points: number of points to use in crossover
-    #	- points_before_death: only consider points upto sooner death
-    #	- normal_dist: sample crossover points from normal distribution
-    ###
-    def crossover_chromosome_pair(self, parent1, parent2, point_num=1, points_before_death=False, normal_dist=False):
-        def positive_normal():
-            x = (np.random.randn() / 3)
-            return max(1 - max(x, -x), 0)
-
-        point_range_max = min(len(parent1[0]), len(parent2[0])) if not points_before_death \
+      point_range_max = min(len(parent1[0]), len(parent2[0])) if not points_before_death \
             else max(parent1[2], parent2[2])
-        random = np.random.rand if not normal_dist else positive_normal
+      random = np.random.rand if not normal_dist else positive_normal
         
-        crossover_points = [point_range_max]
-        for i in range(point_num):
-            crossover_points.append(int(round(point_range_max * random())))
-        crossover_points.sort(reverse=True)
+      crossover_points = [point_range_max]
+      for i in range(point_num):
+         crossover_points.append(int(round(point_range_max * random())))
+      crossover_points.sort(reverse=True)
 
-        child1, child2 = [parent1[0].copy(), -1, -1], [parent2[0].copy(), -1, -1]
-        while len(crossover_points) > 1:
-            point1 = crossover_points.pop()
-            point2 = crossover_points.pop()
-            child1[0][point1:point2] = parent2[0][point1:point2].copy()
-            child2[0][point1:point2] = parent1[0][point1:point2].copy()
+      child1, child2 = [parent1[0].copy(), -1, -1], [parent2[0].copy(), -1, -1]
+      while len(crossover_points) > 1:
+         point1 = crossover_points.pop()
+         point2 = crossover_points.pop()
+         child1[0][point1:point2] = parent2[0][point1:point2].copy()
+         child2[0][point1:point2] = parent1[0][point1:point2].copy()
 
-        return [child1, child2]
+      return [child1, child2]
 
-    ###
-    # Implementation of mutation operator
-    # Mutations: number of mutations to make
-    ###
-    def mutate_chromosome(self, chromosome, mutations=1):
-        for i in range(mutations):
-            #mutation index: triangular distribution with: bound left=0, bound right and mode=index of game over
-            mutation_index = int(np.ceil(np.random.triangular(left=0, mode=1, right=1) * chromosome[2]))
-            #new_action: random within the allowed possible actions
-            actions_size = len(self.action_encoding)
-            #new_action = np.random.randint(low=0, high=actions_size)
-            #new: prioritize actions between 1 to 4 (going right)
-            new_action = np.random.randint(low=0, high=(actions_size+4))
-            if (new_action >= actions_size):
-                new_action = new_action - actions_size + 1
-            chromosome[0][mutation_index] = new_action
-        chromosome[1], chromosome[2] = -1, -1
+   def mutate_chromosome(self, chromosome, mutations=1):
+   ###
+   # Implementation of mutation operator
+   # Mutations: number of mutations to make
+   ###
+      for i in range(int(mutations)):
+         #mutation index: triangular distribution with: bound left=0, bound right and mode=index of game over
+         mutation_index = int(np.ceil(np.random.triangular(left=0, mode=1, right=1) * chromosome[2]))
+         #new_action: random within the allowed possible actions
+         actions_size = len(self.action_encoding)
+         #new_action = np.random.randint(low=0, high=actions_size)
+         #new: prioritize actions between 1 to 4 (going right)
+         new_action = np.random.randint(low=0, high=(actions_size+4))
+         if (new_action >= actions_size):
+            new_action = new_action - actions_size + 1
+         chromosome[0][mutation_index] = new_action
+      chromosome[1], chromosome[2] = -1, -1
--- msb_genetic_optimizer_env.py
+++ msb_genetic_optimizer_env.py
@@ -21,211 +21,211 @@ except:
 #MSBGeneticOptimizerEnv contains boilerplate code for the project, shouldn't need to modify this
 #If you do let me know or make a PR and I can fix it so that others have updated code too
 class MSBGeneticOptimizerEnv(object):
-	"""An environment wrapper for genetically optimizing mario smash brothers simulation ."""
+   """An environment wrapper for genetically optimizing mario smash brothers simulation ."""
 
-	def __init__(self, max_steps=10000, num_chromosomes=4, action_encoding=SIMPLE_MOVEMENT, render=False, fitness_strategy="x_pos", session_file="", world=1, stage=1, version=0, noFrameSkip=False):
-		if session_file != "":
-			self.load_optimizer(session_file)
-		else:
-			self.max_steps = max_steps
-			self.action_encoding = action_encoding
-			self.render = render
-			self.fitness_strategy = fitness_strategy  #score, x_pos, time, coins
-			self.num_chromosomes = num_chromosomes
-			self.world = world
-			self.stage = stage
-			self.version = version
-			self.noFrameSkip = 'NoFrameskip' if noFrameSkip else ''
-			self.init_chromosomes()
+   def __init__(self, max_steps=10000, num_chromosomes=4, action_encoding=SIMPLE_MOVEMENT, render=False, fitness_strategy="x_pos", session_file="", world=1, stage=1, version=0, noFrameSkip=False):
+      if session_file != "":
+         self.load_optimizer(session_file)
+      else:
+         self.max_steps = max_steps
+         self.action_encoding = action_encoding
+         self.render = render
+         self.fitness_strategy = fitness_strategy  #score, x_pos, time, coins
+         self.num_chromosomes = num_chromosomes
+         self.world = world
+         self.stage = stage
+         self.version = version
+         self.noFrameSkip = 'NoFrameskip' if noFrameSkip else ''
+         self.init_chromosomes()
 
-	def init_chromosomes(self):
-		"""Creates a new set of genes based on the number of parents fed in"""
-		self.chromosomes = []
-		for i in range(self.num_chromosomes):
-			chromosome = [np.random.randint(0,len(self.action_encoding), self.max_steps), -1, -1]
-			self.chromosomes.append(chromosome)
-		self.evaluate_chromosomes()
+   def init_chromosomes(self):
+      """Creates a new set of genes based on the number of parents fed in"""
+      self.chromosomes = []
+      for i in range(self.num_chromosomes):
+         chromosome = [np.random.randint(0,len(self.action_encoding), self.max_steps), -1, -1]
+         self.chromosomes.append(chromosome)
+      self.evaluate_chromosomes()
 
-	def save_optimizer(self, fname):
-		print("saving optimizer state to ",fname)
-		optimizer_state = Optimizer(self.max_steps, self.num_chromosomes, self.action_encoding, self.render, self.fitness_strategy, self.chromosomes, self.world, self.stage, self.version, self.noFrameSkip)
-		with open(fname, "wb") as f:
-			pickle.dump(optimizer_state, f)
+   def save_optimizer(self, fname):
+      print("saving optimizer state to ",fname)
+      optimizer_state = Optimizer(self.max_steps, self.num_chromosomes, self.action_encoding, self.render, self.fitness_strategy, self.chromosomes, self.world, self.stage, self.version, self.noFrameSkip)
+      with open(fname, "wb") as f:
+         pickle.dump(optimizer_state, f)
 
-	def load_optimizer(self, fname):
-		print("loading optimier state from ",fname)
-		with open(fname, "rb") as f:
-			optimizer = pickle.load(f)
-			self.max_steps = optimizer.max_steps
-			self.action_encoding = optimizer.action_encoding
-			self.render = optimizer.render
-			self.fitness_strategy = optimizer.fitness_strategy
-			self.num_chromosomes = optimizer.num_chromosomes
-			self.chromosomes = optimizer.chromosomes
-			self.world = optimizer.world
-			self.stage = optimizer.stage
-			self.version = optimizer.version
-			self.noFrameSkip = optimizer.noFrameSkip
-
-
-	def run_generations(self, ngens, fname):
-
-		headers = ['generation', 'chromosome_num', self.fitness_strategy]
-
-		#If logging for the first time, set up csv file
-		if fname:
-			print("logging progress to " + fname)
-
-			with open (fname, 'w') as csvfile:
-				writer = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n',fieldnames=headers)
-				writer.writeheader()
-
-		for gen in range(ngens):
-			self.new_generation()
-			self.evaluate_chromosomes()
-			max_fitness, max_fitness_ix = self.get_max_fitness_chromosome()
-
-			#If writing progress to output, add this generation
-			if fname:
-				with open (fname, 'a') as csvfile:
-					writer = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n',fieldnames=headers)
-					writer.writerow({'generation': gen, 'chromosome_num': max_fitness_ix, self.fitness_strategy: max_fitness})
-
-			print("\n#################################")
-			print("GENERATION",gen,"COMPLETE")
-			print("Highest chromosome: ",max_fitness_ix,", fitness:",max_fitness)
-			print("####################################\n\n\n")
+   def load_optimizer(self, fname):
+      print("loading optimier state from ",fname)
+      with open(fname, "rb") as f:
+         optimizer = pickle.load(f)
+         self.max_steps = optimizer.max_steps
+         self.action_encoding = optimizer.action_encoding
+         self.render = optimizer.render
+         self.fitness_strategy = optimizer.fitness_strategy
+         self.num_chromosomes = optimizer.num_chromosomes
+         self.chromosomes = optimizer.chromosomes
+         self.world = optimizer.world
+         self.stage = optimizer.stage
+         self.version = optimizer.version
+         self.noFrameSkip = optimizer.noFrameSkip
 
 
-	def get_max_fitness_chromosome(self):
-		"""returns highest fitness of current chromosomes, along with its index"""
-		max_fitness = -1
-		max_fitness_ix = -1
-		max_chromosome = []
-		for cix, chromosome in enumerate(self.chromosomes):
-			if chromosome[1] > max_fitness:
-				max_fitness = chromosome[1]
-				max_fitness_ix = cix
+   def run_generations(self, ngens, fname):
 
-		return max_fitness, max_fitness_ix
+      headers = ['generation', 'chromosome_num', self.fitness_strategy]
 
-	@abstractmethod
-	def new_generation(self):
-		"""
-		Based on a chromosomes structure, updates the chromosomes by natural selection rules
-		This is where the bulk of the evolutionary computation code will go
-		Update here
-		"""
-		#
-		pass
-		# For now now selection occurs, just keep current chromosomes
+      #If logging for the first time, set up csv file
+      if fname:
+         print("logging progress to " + fname)
 
+         with open (fname, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n',fieldnames=headers)
+            writer.writeheader()
 
-	def run_top_chromosome(self, render=False):
-		"""
-		Retrieve the best-performing chromosome and play it.
-		Override render argument in case only want to visualize on test round
-		"""
-		max_fitness, max_fitness_ix = self.get_max_fitness_chromosome()
+      for gen in range(ngens):
+         self.new_generation()
+         self.evaluate_chromosomes()
+         max_fitness, max_fitness_ix = self.get_max_fitness_chromosome()
 
-		if max_fitness == -1:
-			print('Run top chromosome error: no fitnesses have been computed')
+         #If writing progress to output, add this generation
+         if fname:
+            with open (fname, 'a') as csvfile:
+               writer = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n',fieldnames=headers)
+               writer.writerow({'generation': gen, 'chromosome_num': max_fitness_ix, self.fitness_strategy: max_fitness})
 
-		with mariocontext(self) as env:
-			done = True
-			for step, action in enumerate(self.chromosomes[max_fitness_ix][0]):
-					if done:
-						state = env.reset()
-
-					state, reward, done, info = env.step(action)
-
-					if render: env.render()
+         print("\n#################################")
+         print("GENERATION",gen,"COMPLETE")
+         print("Highest chromosome: ",max_fitness_ix,", fitness:",max_fitness)
+         print("####################################\n\n\n")
 
 
-	def evaluate_chromosome(self, input_tuple):
-		"""Evaluates a chromosome for it's fitness value and index of death"""
+   def get_max_fitness_chromosome(self):
+      """returns highest fitness of current chromosomes, along with its index"""
+      max_fitness = -1
+      max_fitness_ix = -1
+      max_chromosome = []
+      for cix, chromosome in enumerate(self.chromosomes):
+         if chromosome[1] > max_fitness:
+            max_fitness = chromosome[1]
+            max_fitness_ix = cix
 
-		chromosome_num, chromosome = input_tuple
+      return max_fitness, max_fitness_ix
 
-		if chromosome[1] != -1:
-			return chromosome
-
-		with mariocontext(self) as env:
-
-			state = env.reset()
-			#Main evaluation loop for this chromosome
-			for step, action in enumerate(chromosome[0]):
-
-				#take step
-				state, reward, done, info = env.step(action)
-
-				#died or level beat
-				if done:
-					break
-
-				#print progress
-				if step % 50 == 0:
-					print("chromosome:",chromosome_num," step:", step," action:",action, "info:",info)
-
-				#display on screen
-				if self.render:
-					env.render()
-
-			chromosome[1], chromosome[2] = info[self.fitness_strategy], step
-
-			print("chromosome",chromosome_num," done fitness ",self.fitness_strategy ,"= ",info[self.fitness_strategy])
-			return chromosome
+   @abstractmethod
+   def new_generation(self):
+      """
+      Based on a chromosomes structure, updates the chromosomes by natural selection rules
+      This is where the bulk of the evolutionary computation code will go
+      Update here
+      """
+      #
+      pass
+      # For now now selection occurs, just keep current chromosomes
 
 
-	def evaluate_chromosomes(self):
-		"""
-		Given a gene structure, evaluates all genes for their fitness and stores it in the stucture
-		This is what actually runs the training simulation
-		Input: a gene structure with (possibly) empty fitnesses
-		Output: a gene structure with computed fitnesses and index of death
-		"""
+   def run_top_chromosome(self, render=False):
+      """
+      Retrieve the best-performing chromosome and play it.
+      Override render argument in case only want to visualize on test round
+      """
+      max_fitness, max_fitness_ix = self.get_max_fitness_chromosome()
 
-		bound_instance_method_alias = functools.partial(_instance_method_alias, self)
-		with poolcontext(multiprocessing.cpu_count()) as pool:
-			self.chromosomes = pool.map(bound_instance_method_alias, enumerate(self.chromosomes))
+      if max_fitness == -1:
+         print('Run top chromosome error: no fitnesses have been computed')
+
+      with mariocontext(self) as env:
+         done = True
+         for step, action in enumerate(self.chromosomes[max_fitness_ix][0]):
+               if done:
+                  state = env.reset()
+
+               state, reward, done, info = env.step(action)
+
+               if render: env.render()
+
+
+   def evaluate_chromosome(self, input_tuple):
+      """Evaluates a chromosome for it's fitness value and index of death"""
+
+      chromosome_num, chromosome = input_tuple
+
+      if chromosome[1] != -1:
+         return chromosome
+
+      with mariocontext(self) as env:
+
+         state = env.reset()
+         #Main evaluation loop for this chromosome
+         for step, action in enumerate(chromosome[0]):
+
+            #take step
+            state, reward, done, info = env.step(action)
+
+            #died or level beat
+            if done:
+               break
+
+            #print progress
+            if step % 50 == 0:
+               print("chromosome:",chromosome_num," step:", step," action:",action, "info:",info)
+
+            #display on screen
+            if self.render:
+               env.render()
+
+         chromosome[1], chromosome[2] = info[self.fitness_strategy], step
+
+         print("chromosome",chromosome_num," done fitness ",self.fitness_strategy ,"= ",info[self.fitness_strategy])
+         return chromosome
+
+
+   def evaluate_chromosomes(self):
+      """
+      Given a gene structure, evaluates all genes for their fitness and stores it in the stucture
+      This is what actually runs the training simulation
+      Input: a gene structure with (possibly) empty fitnesses
+      Output: a gene structure with computed fitnesses and index of death
+      """
+
+      bound_instance_method_alias = functools.partial(_instance_method_alias, self)
+      with poolcontext(multiprocessing.cpu_count()) as pool:
+         self.chromosomes = pool.map(bound_instance_method_alias, enumerate(self.chromosomes))
 
 
 class Optimizer():
-	"""A basic container class for saving optimizer contents"""
+   """A basic container class for saving optimizer contents"""
 
-	def __init__(self, max_steps, num_chromosomes, action_encoding, render, fitness_strategy, chromosomes, world, stage, version, noFrameSkip):
-		self.max_steps = max_steps
-		self.num_chromosomes = num_chromosomes
-		self.action_encoding = action_encoding
-		self.render = render
-		self.fitness_strategy = fitness_strategy
-		self.chromosomes = chromosomes
-		self.world = world
-		self.stage = stage
-		self.version = version
-		self.noFrameSkip = noFrameSkip
+   def __init__(self, max_steps, num_chromosomes, action_encoding, render, fitness_strategy, chromosomes, world, stage, version, noFrameSkip):
+      self.max_steps = max_steps
+      self.num_chromosomes = num_chromosomes
+      self.action_encoding = action_encoding
+      self.render = render
+      self.fitness_strategy = fitness_strategy
+      self.chromosomes = chromosomes
+      self.world = world
+      self.stage = stage
+      self.version = version
+      self.noFrameSkip = noFrameSkip
 
 
 @contextmanager
 def poolcontext(*args, **kwargs):
-	pool = multiprocessing.Pool(*args, **kwargs)
-	yield pool
-	pool.terminate()
+   pool = multiprocessing.Pool(*args, **kwargs)
+   yield pool
+   pool.terminate()
 
 @contextmanager
 def mariocontext(marioEnv):
-	mario_env = 'SuperMarioBros' + marioEnv.noFrameSkip + '-' + str(marioEnv.world) + '-' + str(marioEnv.stage) + '-v' + str(marioEnv.version)
-	env = gym_super_mario_bros.make(mario_env)
-	env = BinarySpaceToDiscreteSpaceEnv(env, marioEnv.action_encoding)
-	yield env
-	env.close()
+   mario_env = 'SuperMarioBros' + marioEnv.noFrameSkip + '-' + str(marioEnv.world) + '-' + str(marioEnv.stage) + '-v' + str(marioEnv.version)
+   env = gym_super_mario_bros.make(mario_env)
+   env = BinarySpaceToDiscreteSpaceEnv(env, marioEnv.action_encoding)
+   yield env
+   env.close()
 
 
 def _instance_method_alias(obj, arg):
-	"""
-	Alias for instance method that allows the method to be called in a
-	multiprocessing pool
-	"""
-	return obj.evaluate_chromosome(arg)
+   """
+   Alias for instance method that allows the method to be called in a
+   multiprocessing pool
+   """
+   return obj.evaluate_chromosome(arg)
 
 
